9b20d34bbc1c982aca2f0d9f592cbc5bbd212f4c
Akash Godbole <akash.godbole16@gmail.com>
2020-05-21 13:32:10 -0400

    Trying to figure out correct porting

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def interactive():
 def main():
     from db import db
     db.init_app(app)
-    app.run(host="0.0.0.0", port=8000, threaded=True)
+    app.run(host="3.14.6.168", port=8000, threaded=True)
 
 
 if __name__ == "__main__":
